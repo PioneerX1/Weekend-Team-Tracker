b8231ddf3eb0884c451eb1ec67e7342cd5278dd6
Mick Sexton <mick@>
2017-07-07 09:13:09 -0700

    add all remaining member varables and custom methods to Member class as well as Member tests

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,7 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
   private String mName;
+  private int mId;
+  private static List<Member> instances = new ArrayList<Member>();
 
   public Member(String name) {
     mName = name;
+    instances.add(this);
+    mId = instances.size();
+  }
+
+  public String getMemberName() {
+    return mName;
+  }
+
+  public int getMemberId() {
+    return mId;
+  }
+
+  public static List<Member> getAllMembers() {
+    return instances;
+  }
+
+  public static void clearAllMembers() {
+    instances.clear();
+  }
+
+  public static Member findMember(int id) {
+    return instances.get(id - 1);
   }
 }
--- src/test/java/MemberTest.java
+++ src/test/java/MemberTest.java
@@ -2,10 +2,52 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class MemberTest {
+  @Before
+  public void reset() {
+    Member.clearAllMembers();
+  }
+
   @Test
   public void Member_instantiatesCorrectly_true() {
     Member testMember = new Member("Fred");
     assertEquals(true, testMember instanceof Member);
+  }
+  @Test
+  public void getMemberName_retrieveMemberName_Fred() {
+    Member testMember = new Member("Fred");
+    assertEquals("Fred", testMember.getMemberName());
+  }
+  @Test
+  public void getAllMembers_retrieveAllInstancesOfMember_true() {
+    Member testMember = new Member("Fred");
+    Member testMember2 = new Member("Chuck");
+    assertEquals(true, Member.getAllMembers().contains(testMember));
+    assertEquals(true, Member.getAllMembers().contains(testMember2));
+  }
+  @Test
+  public void clearAllMembers_eraseAllInstancesOfMember_0() {
+    Member testMember = new Member("Fred");
+    Member testMember2 = new Member("Chuck");
+    Member.clearAllMembers();
+    assertEquals(0, Member.getAllMembers().size());
+  }
+  @Test
+  public void getMemberId_retrieveUniqueMemberId_2() {
+    Member testMember = new Member("Fred");
+    Member testMember2 = new Member("Chuck");
+    assertEquals(2, testMember2.getMemberId());
+  }
+  @Test
+  public void getAllMembers_retrieveNumberOfTotalMembers_2() {
+    Member testMember = new Member("Fred");
+    Member testMember2 = new Member("Chuck");
+    assertEquals(2, Member.getAllMembers().size());
+  }
+  @Test
+  public void findMember_retrievesMemberWithSameId_Fred() {
+    Member testMember = new Member("Fred");
+    Member testMember2 = new Member("Chuck");
+    assertEquals(testMember, Member.findMember(testMember.getMemberId()));
   }
 
 }
