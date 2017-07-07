74670cb30485bb2f41246382c4e67192e8f7c00d
Mick Sexton <mick@>
2017-07-07 10:33:04 -0700

    add App routes and templates for /teams and /teams/:id pages

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,6 +13,29 @@ public class App {
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
+      //if no teams, then make two, each with one team member
+      if (Team.getAllTeams().size() == 0) {
+        Team defaultTeam1 = new Team("The Rogue Yoshi's");
+        Member defaultMember1 = new Member("Luigi");
+        Team defaultTeam2 =  new Team("Twin Peaks");
+        Member defaultMember2 = new Member("Laura Palmer");
+      }
+      model.put("teams", Team.getAllTeams());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/teams", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("teams", Team.getAllTeams());
+      model.put("template", "templates/teams.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/teams/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Team team = Team.findTeam(Integer.parseInt(request.params(":id")));
+      model.put("team", team);
+      model.put("template", "templates/team.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
